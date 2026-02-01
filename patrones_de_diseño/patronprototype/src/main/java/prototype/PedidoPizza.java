f01e657b99c8b583d8bccf0535040cb2a3fca26d
breinner <correo@gmial.com>
2026-02-01 13:15:26 -0500

    patron protorype

--- patrones_de_diseño/patronprototype/src/main/java/prototype/PedidoPizza.java
+++ patrones_de_diseño/patronprototype/src/main/java/prototype/PedidoPizza.java
@@ -2,15 +2,44 @@ package prototype;
 
 import java.util.List;
 
-public class PedidoPizza implements  PrototipoPizza <PedidoPizza>{
+public class PedidoPizza implements PrototipoPizza<PedidoPizza> {
 
     private String tamanio;
     private String tipoMasa;
-    private List<String>
+    private List<String> ingredientes;
 
+    // constuctor normal (crear desde cero )
+    public PedidoPizza(String tamanio, String tipoMasam, List<String> ingredientes) {
+        this.tamanio = tamanio;
+        this.tipoMasa = tipoMasa;
+        this.ingredientes = ingredientes;
+    }
+
+    // constructor copia
+    public PedidoPizza(PedidoPizza prototipo) {
+        this.tamanio = prototipo.tamanio;
+        this.tipoMasa = prototipo.tipoMasa;
+        this.ingredientes = prototipo.ingredientes;
+    }
 
     @Override
     public PedidoPizza clonar() {
-        return null;
+        return new PedidoPizza(this);
+    }
+
+    // es un metodo para agregar los ingredientes
+
+    public void agregarIngredientes(String ingrefientes) {
+        this.ingredientes.add(ingrefientes);
+
+    }
+
+    @Override
+    public String toString() {
+        return "PedidoPizza{" +
+                "tamanio='" + tamanio + '\'' +
+                ", tipoMasa='" + tipoMasa + '\'' +
+                ", ingredientes=" + ingredientes +
+                '}';
     }
 }
