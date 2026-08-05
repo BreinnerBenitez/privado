409b3deb9bf9950b5f1c50b79824a3f01ecacad6
breinner <jbreinner_inge@hotmail.com>
2026-08-04 23:20:33 -0500

    cambios para encriptar contraseña

--- Security/src/main/java/com/todocode/springsecurity/service/IUserSec.java
+++ Security/src/main/java/com/todocode/springsecurity/service/IUserSec.java
@@ -18,5 +18,6 @@ public interface IUserSec {
 
     public void update(UserSec userSec);
 
+    String encriptPassword(String password);
 
 }
--- Security/src/main/java/com/todocode/springsecurity/service/UserSecService.java
+++ Security/src/main/java/com/todocode/springsecurity/service/UserSecService.java
@@ -4,6 +4,7 @@ import com.todocode.springsecurity.model.Role;
 import com.todocode.springsecurity.model.UserSec;
 import com.todocode.springsecurity.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -41,6 +42,10 @@ public class UserSecService implements IUserSec {
 
     }
 
+    @Override
+    public String encriptPassword(String password) {
+        return new BCryptPasswordEncoder().encode(password);
+    }
 
 
 }
